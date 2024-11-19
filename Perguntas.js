470d56f4dea2a13bee5c61ed86edd4814cd60b1c
Dark8756 <137402022+Dark8756@users.noreply.github.com>
2024-11-19 09:01:43 -0300

    Add files via upload

---
+++ Perguntas.js
@@ -0,0 +1,24 @@
+criarCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criarCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris',
+)
+
+criarCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa',
+)
+
+criarCartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+    
+)
